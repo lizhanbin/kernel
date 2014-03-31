253cb4835e393a27a4022fd263605b6799a770b1
lizhanbin <lizhanbin0214@gmail.com>
2014-03-31 22:04:03 +0800

    Create mymain.c

---
+++ mymain.c
@@ -0,0 +1,75 @@
+/*
+ *  linux/mykernel/mymain.c
+ *
+ *  Kernel internal my_start_kernel
+ *
+ *  Copyright (C) 2013  lizhanbin
+ *
+ */
+#include <linux/types.h>
+#include <linux/string.h>
+#include <linux/ctype.h>
+#include <linux/tty.h>
+#include <linux/vmalloc.h>
+
+
+#include "mypcb.h"
+
+tPCB task[MAX_TASK_NUM];
+tPCB * my_current_task = NULL;
+volatile int my_need_sched = 0;
+
+void my_process(void);
+
+
+void __init my_start_kernel(void)
+{
+    int pid = 0;
+    int i;
+    /* Initialize process 0*/
+    task[pid].pid = pid;
+    task[pid].state = 0;/* -1 unrunnable, 0 runnable, >0 stopped */
+    task[pid].task_entry = task[pid].thread.ip = (unsigned long)my_process;//进程0为主进程，其ip为入口地址，即my_process
+    task[pid].thread.sp = (unsigned long)&task[pid].stack[KERNEL_STACK_SIZE-1];
+    task[pid].next = &task[pid];
+    /*fork more process */
+    for(i=1;i<MAX_TASK_NUM;i++)
+    {
+        memcpy(&task[i],&task[0],sizeof(tPCB));
+        task[i].pid = i;
+        task[i].state = -1;
+        task[i].thread.sp = (unsigned long)&task[i].stack[KERNEL_STACK_SIZE-1];
+        task[i].next = task[i-1].next; /*链表insert操作*/
+        task[i-1].next = &task[i];
+    }
+    /* start process 0 by task[0] */
+    pid = 0;
+    my_current_task = &task[pid];
+	asm volatile(
+    	"movl %1,%%esp\n\t" 	/* set task[pid].thread.sp to esp */
+    	"pushl %1\n\t" 	        /* push ebp */
+    	"pushl %0\n\t" 	        /* push task[pid].thread.ip */
+    	"ret\n\t" 	            /* pop task[pid].thread.ip to eip */
+    	"popl %%ebp\n\t"
+    	: 
+    	: "c" (task[pid].thread.ip),"d" (task[pid].thread.sp)	/* input c or d mean %ecx/%edx*/
+	);
+}   
+void my_process(void)
+{
+    int i = 0;
+    while(1)
+    {
+        i++;
+        if(i%10000000 == 0)
+        {
+            printk(KERN_NOTICE "this is process %d -\n",my_current_task->pid);
+            if(my_need_sched == 1)//我需要调度
+            {
+                my_need_sched = 0;//我正在被调度，不可再调度
+        	    my_schedule();//进行调度
+        	}
+        	printk(KERN_NOTICE "this is process %d +\n",my_current_task->pid);
+        }     
+    }
+}
